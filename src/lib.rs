74906077f15cd6cecce9d29341d6013c2f78c448
shivanandvp <shivanandvp.oss@gmail.com>
2022-06-04 06:43:17 -0500

    PCI ID formats added

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,7 @@ pub mod commandline {
     use crate::*;
     use clap::Parser;
     use serde::Serialize;
+    use std::ops::RangeInclusive;
 
     pub enum CommandlineOutputKind {
         Regular,
@@ -90,7 +91,10 @@ pub mod commandline {
                     let db = DriverDatabase::load_from_path_or_default("test.ron").unwrap();
                     println!("Writing to Database");
                     db.write(|db| {
-                        db.insert(0x12345678..=0x123456ab, vec![DriverRecord::default()]);
+                        db.insert(
+                            PciId::range_inclusive("1234:5678", "1234:56ab").expect("Invalid PCI IDs supplied"),
+                            vec![DriverRecord::default()],
+                        );
                         println!("Entries: \n{:#?}", db);
                     })
                     .unwrap();
@@ -238,31 +242,157 @@ pub mod commandline {
 }
 
 pub mod data {
-    use rangemap::RangeInclusiveMap;
+    use rangemap::{RangeInclusiveMap, StepLite};
     use rustbreak::{deser::Ron, FileDatabase};
-    use serde::{Deserialize, Serialize};
-    use std::{collections::HashMap, path::PathBuf, fmt::Display};
+    use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+    use std::num::ParseIntError;
+    use std::{
+        collections::HashMap,
+        fmt::{Debug, Display},
+        ops::{Range, RangeInclusive},
+        path::PathBuf,
+        str::FromStr,
+    };
 
     // pub type DriverListing = RangeInclusiveMap<u32, Vec<DriverRecord>>;
-    pub type DriverListing = RangeInclusiveMap<u32, Vec<DriverRecord>>;
+    pub type DriverListing = RangeInclusiveMap<PciId, Vec<DriverRecord>>;
     pub type DriverDatabase = FileDatabase<DriverListing, Ron>;
 
     #[derive(
         Default,
-        PartialEq, // Required to implement Eq
-        Eq,        // Required by RangeInclusiveMap to implement Serialize and Deserialize
+        PartialEq,
+        Eq,
+        PartialOrd, // Required by Ord
+        Ord,        // Required by RangeInclusiveMap to implement Serialize and Deserialize
         Copy,
-        Clone,     // Required by RangeInclusiveMap to implement Serialize and Deserialize
-        Serialize,
-        Deserialize,
+        Clone, // Required by RangeInclusiveMap to implement Serialize and Deserialize
     )]
     pub struct PciId {
-        pub inner: u32,
+        value: u32,
+    }
+
+    impl PciId {
+        pub fn vendor_id(&self) -> u16 {
+            let vendor_id = self.value % 2u32.pow(4);
+            vendor_id
+                .try_into()
+                .expect("The Vendor ID does not fit into an unsigned 16-bit integer.")
+        }
+        pub fn device_id(&self) -> u16 {
+            let device_id = self.value / 2u32.pow(4);
+            device_id
+                .try_into()
+                .expect("The Device ID does not fit into an unsigned 16-bit integer.")
+        }
+        pub fn range(start: &str, end: &str) -> Result<Range<Self>, ParsePciIdError> {
+            Ok(Range {
+                start: start.parse()?,
+                end: end.parse()?,
+            })
+        }
+        pub fn range_inclusive(
+            start: &str,
+            end: &str,
+        ) -> Result<RangeInclusive<Self>, ParsePciIdError> {
+            Ok(RangeInclusive::new(start.parse()?, end.parse()?))
+        }
     }
 
     impl Display for PciId {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{}", self.inner)
+            write!(f, "{:04}:{:04}", self.vendor_id(), self.device_id())
+        }
+    }
+
+    impl Debug for PciId {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("PciId")
+                .field("vendor_id", &format!("{:04}", &self.vendor_id()))
+                .field("device_id", &format!("{:04}", &self.device_id()))
+                .finish()
+        }
+    }
+
+    impl Serialize for PciId {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            serializer.serialize_str(&format!("{}", self))
+        }
+    }
+
+    impl<'de> Deserialize<'de> for PciId {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            struct Visitor;
+            impl<'de> de::Visitor<'de> for Visitor {
+                type Value = PciId;
+                fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    write!(formatter, "a PCI ID")
+                }
+                fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+                where
+                    E: de::Error,
+                {
+                    v.parse().map_err(E::custom)
+                }
+            }
+            deserializer.deserialize_str(Visitor)
+        }
+    }
+
+    impl StepLite for PciId {
+        fn add_one(&self) -> Self {
+            Self {
+                value: self.value + 1,
+            }
+        }
+
+        fn sub_one(&self) -> Self {
+            Self {
+                value: self.value - 1,
+            }
+        }
+    }
+
+    impl FromStr for PciId {
+        type Err = ParsePciIdError;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let (vendor_id, device_id) = s.split_once(':').ok_or(ParsePciIdError::MissingColon)?;
+            let vendor_id = u32::from_str_radix(vendor_id, 16)
+                .map_err(|parse_int_error| ParsePciIdError::InvalidVendorId(parse_int_error))?;
+            let device_id = u32::from_str_radix(device_id, 16)
+                .map_err(|parse_int_error| ParsePciIdError::InvalidDeviceId(parse_int_error))?;
+            Ok(Self {
+                value: vendor_id * 2u32.pow(4) + device_id,
+            })
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum ParsePciIdError {
+        InvalidVendorId(ParseIntError),
+        InvalidDeviceId(ParseIntError),
+        MissingColon,
+    }
+
+    impl Display for ParsePciIdError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                ParsePciIdError::InvalidVendorId(parse_int_error) => {
+                    write!(f, "Invalid Vendor ID. Please refer to {}", parse_int_error)
+                }
+                ParsePciIdError::InvalidDeviceId(parse_int_error) => {
+                    write!(f, "Invalid Device ID. Please refer to {}", parse_int_error)
+                }
+                ParsePciIdError::MissingColon => {
+                    write!(f, "Invalid PCI ID. Please ensure that the Vendor and Device IDs are separated by a colon `:`")
+                }
+            }
         }
     }
 
